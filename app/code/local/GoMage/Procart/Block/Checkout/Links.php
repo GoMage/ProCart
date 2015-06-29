4e00728ae4afec4764b1500cd26f09d2259abb72
gm-ip <ip@gomage.com>
2015-06-29 19:17:43 +0300

    gomage-2364

---
+++ app/code/local/GoMage/Procart/Block/Checkout/Links.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * GoMage Procart Extension
+ *
+ * @category     Extension
+ * @copyright    Copyright (c) 2010-2014 GoMage (http://www.gomage.com)
+ * @author       GoMage
+ * @license      http://www.gomage.com/license-agreement/  Single domain license
+ * @terms of use http://www.gomage.com/terms-of-use
+ * @version      Release: 2.1
+ * @since        Class available since Release 2.2
+ */
+ 
+class GoMage_Procart_Block_Checkout_Links extends Mage_Checkout_Block_Links
+{
+	public function addCartLink()
+	{
+		$helper = Mage::helper('gomage_procart');
+		
+		if (!$helper || !$helper->isProCartEnable()) {
+			return parent::addCartLink();
+		}
+		
+		$parentBlock = $this->getParentBlock();
+		
+		if ($parentBlock && Mage::helper('core')->isModuleOutputEnabled('Mage_Checkout')) {
+			$count = $this->getSummaryQty() 
+				? $this->getSummaryQty()
+					: $this->helper('checkout/cart')->getSummaryCount();
+			
+			if ($count == 1) {
+				$text = $this->__('My Cart (%s item)', $count);
+			} elseif ($count > 0) {
+				$text = $this->__('My Cart (%s items)', $count);
+			} else {
+				$text = $this->__('My Cart');
+			}
+			
+			$parentBlock->removeLinkByUrl($this->getUrl('checkout/cart'));
+			
+			$before		= '';
+			$sidebar	= $parentBlock->getLayout()
+				->createBlock('checkout/cart_sidebar')
+				->setName('cart_sidebar')
+				->setTemplate('checkout/cart/sidebar.phtml')
+				->toHtml();
+			$after		= '<div id="gpc-minicart" style="display: none;">' . $sidebar . '</div>';
+			
+			$parentBlock->addLink($text, '', $text, true, array(), 50, null, 'class="top-link-cart skip-link skip-cart" onclick="$(\'gpc-minicart\').toggle(); return false;"', $before, $after);
+		}
+		
+		return $this;
+	}
+}
